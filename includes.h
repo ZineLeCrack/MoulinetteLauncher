d3f9178f6693810813227f09491fa7713b9922f5
ZineLeCrack <rlebaillif27@gmail.com>
2025-09-13 15:34:12 +0200

    C00/ex03 + C00/ex04

--- includes.h
+++ includes.h
@@ -3,5 +3,6 @@
 # include <string.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include <limits.h>
 # include <math.h>
 # include <sys/wait.h>
--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@
 RED="\033[31;1m"
 GREEN="\033[32;1m"
 YELLOW="\033[33;1m"
+BLUE="\033[34;1m"
+MAGENTA="\033[35;1m"
 RESET="\033[0m"
 
 echo -e "$YELLOW====================== Moulinette Launcher ======================$RESET"
