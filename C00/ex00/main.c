c38761ce22fd342beb9110c9e5efc04995b886ee
ZineLeCrack <rlebaillif27@gmail.com>
2025-09-13 16:35:03 +0200

    improvement

--- C00/ex00/main.c
+++ C00/ex00/main.c
@@ -4,16 +4,22 @@ void	ft_putchar(char c);
 
 int main()
 {
+	ft_putstr("Testing ' ': ");
 	ft_putchar(' ');
 	ft_putchar('\n');
+	ft_putstr("Testing '4': ");
 	ft_putchar('4');
 	ft_putchar('\n');
+	ft_putstr("Testing '2': ");
 	ft_putchar('2');
 	ft_putchar('\n');
+	ft_putstr("Testing '~': ");
 	ft_putchar('~');
 	ft_putchar('\n');
+	ft_putstr("Testing DEL: ");
 	ft_putchar(127);
 	ft_putchar('\n');
+	ft_putstr("Testing NON ASCII CHAR: ");
 	ft_putchar(-128);
 	ft_putchar('\n');
 }
