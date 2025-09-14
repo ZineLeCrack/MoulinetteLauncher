5c024f212ea69dfc20b40c3a6ba705ba9a9426b5
ZineLeCrack <rlebaillif27@gmail.com>
2025-09-14 11:02:33 +0200

    C02/ex01

--- C02/ex01/main.c
+++ C02/ex01/main.c
@@ -1,16 +1,15 @@
 #include "../../includes.h"
 
-char	*ft_strcpy(char *dest, char *src);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
 
 int	main()
 {
 	char	dest1[] = "Hello";
 	char	src1[] = "World";
 
-	printf("dest = \"%s\", src = \"%s\"  -->  ", dest1, src1);
-	char *return_value = ft_strcpy(dest1, src1);
+	printf("dest = \"%s\", src = \"%s\", n = 6  -->  ", dest1, src1);
+	char *return_value = ft_strncpy(dest1, src1, 6);
 	printf("dest = \"%s\", returned value = %s\n", dest1, return_value == dest1 ? "OK" : "KO");
-
 
 
 	char	dest2[] = "123456789";
@@ -20,8 +19,8 @@ int	main()
 	putstr(dest2);
 	putstr("\", src = \"");
 	putstr(src2);
-	putstr("\"  -->  ");
-	return_value = ft_strcpy(dest2, src2);
+	putstr("\", n = 7  -->  ");
+	return_value = ft_strncpy(dest2, src2, 7);
 	putstr("dest = \"");
 	for (int i = 0; i < 9; i++) {
 		if (!dest2[i]) write(1, "\\0", 2);
@@ -36,8 +35,8 @@ int	main()
 	char	dest3[] = "";
 	char	src3[] = "";
 
-	printf("dest = \"%s\", src = \"%s\"  -->  ", dest3, src3);
-	return_value = ft_strcpy(dest3, src3);
+	printf("dest = \"%s\", src = \"%s\", n = 2  -->  ", dest3, src3);
+	return_value = ft_strncpy(dest3, src3, 2);
 	printf("dest = \"%s\", returned value = %s\n", dest3, return_value == dest3 ? "OK" : "KO");
 
 
@@ -45,8 +44,8 @@ int	main()
 	char	dest4[] = "42";
 	char	src4[] = "strcpy";
 
-	printf("dest = \"%s\", src = \"%s\"  -->  ", dest4, src4);
-	return_value = ft_strcpy(dest4, src4);
+	printf("dest = \"%s\", src = \"%s\", n = 4  -->  ", dest4, src4);
+	return_value = ft_strncpy(dest4, src4, 4);
 	printf("dest = \"%s\", returned value = %s\n", dest4, return_value == dest4 ? "OK" : "KO");
 
 
@@ -54,8 +53,8 @@ int	main()
 	char	dest5[] = "42";
 	char	src5[] = "";
 
-	printf("dest = \"%s\", src = \"%s\"  -->  ", dest5, src5);
-	return_value = ft_strcpy(dest5, src5);
+	printf("dest = \"%s\", src = \"%s\", n = 1  -->  ", dest5, src5);
+	return_value = ft_strncpy(dest5, src5, 1);
 	printf("dest = \"%s\", returned value = %s\n", dest5, return_value == dest5 ? "OK" : "KO");
 
 
@@ -63,7 +62,7 @@ int	main()
 	char	dest6[] = "";
 	char	src6[] = "42";
 
-	printf("dest = \"%s\", src = \"%s\"  -->  ", dest6, src6);
-	return_value = ft_strcpy(dest6, src6);
+	printf("dest = \"%s\", src = \"%s\", n = 3  -->  ", dest6, src6);
+	return_value = ft_strncpy(dest6, src6, 3);
 	printf("dest = \"%s\", returned value = %s\n", dest6, return_value == dest6 ? "OK" : "KO");
 }
