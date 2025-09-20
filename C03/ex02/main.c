7900f1d7c0d90d5977f63981f060813b9e908601
ZineLeCrack <rlebaillif27@gmail.com>
2025-09-20 12:28:35 +0200

    ft_strcat/ft_strncat repaired

--- C03/ex02/main.c
+++ C03/ex02/main.c
@@ -9,30 +9,26 @@ int	main()
 
 	printf("dest = \"%s\", src = \"%s\"  -->  ", s1, s2);
 	char	*return_value = ft_strcat(s1, s2);
-	printf("dest = \"%s\"  src = \"%s\",returned value = %s\n", s1, s2, return_value == s1 ? "OK" : "KO");
-
-
+	printf("dest = \"%s\", returned value = %s\n", s1, return_value == s1 ? "OK" : "KO");
 
 	char	s3[] = "";
 	char	s4[] = "World";
 
 	printf("dest = \"%s\", src = \"%s\"  -->  ", s3, s4);
 	return_value = ft_strcat(s3, s4);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s3, s4, return_value == s3 ? "OK" : "KO");
-
-
+	printf("dest = \"%s\", returned value = %s\n", s3, return_value == s3 ? "OK" : "KO");
 
 	char	s5[] = "Hello";
 	char	s6[] = "";
 
 	printf("dest = \"%s\", src = \"%s\"  -->  ", s5, s6);
 	return_value = ft_strcat(s5, s6);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s5, s6, return_value == s5 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s5, return_value == s5 ? "OK" : "KO");
 
 	char	s7[] = "";
 	char	s8[] = "";
 
 	printf("dest = \"%s\", src = \"%s\"  -->  ", s7, s8);
 	return_value = ft_strcat(s7, s8);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s7, s8, return_value == s7 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s7, return_value == s7 ? "OK" : "KO");
 }
--- C03/ex03/main.c
+++ C03/ex03/main.c
@@ -9,28 +9,28 @@ int	main()
 
 	printf("dest = \"%s\", src = \"%s\", n = 0  -->  ", str1, str2);
 	char	*return_value = ft_strncat(str1, str2, 0);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", str1, str2, return_value == str1 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", str1, return_value == str1 ? "OK" : "KO");
 
 	char	s1[] = "Hello";
 	char	s2[] = "World";
 
 	printf("dest = \"%s\", src = \"%s\", n = 3  -->  ", s1, s2);
 	return_value = ft_strncat(s1, s2, 3);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s1, s2, return_value == s1 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s1, return_value == s1 ? "OK" : "KO");
 
 	char	s3[] = "Hello";
 	char	s4[] = "World";
 
 	printf("dest = \"%s\", src = \"%s\", n = 5  -->  ", s3, s4);
 	return_value = ft_strncat(s3, s4, 5);
-	printf("dest = \"%s\"  src = \"%s\", returned value = %s\n", s3, s4, return_value == s3 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s3, return_value == s3 ? "OK" : "KO");
 
 	char	s5[] = "Hello";
 	char	s6[] = "World";
 
 	printf("dest = \"%s\", src = \"%s\", n = 7  -->  ", s5, s6);
 	return_value = ft_strncat(s5, s6, 7);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s5, s6, return_value == s5 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s5, return_value == s5 ? "OK" : "KO");
 
 
 
@@ -39,7 +39,7 @@ int	main()
 
 	printf("dest = \"%s\", src = \"%s\", n = 2  -->  ", s7, s8);
 	return_value = ft_strncat(s7, s8, 2);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s7, s8, return_value == s7 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s7, return_value == s7 ? "OK" : "KO");
 
 
 
@@ -48,7 +48,7 @@ int	main()
 
 	printf("dest = \"%s\", src = \"%s\", n = 2  -->  ", s9, s10);
 	return_value = ft_strncat(s9, s10, 2);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s9, s10, return_value == s9 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s9, return_value == s9 ? "OK" : "KO");
 
 
 
@@ -57,5 +57,5 @@ int	main()
 
 	printf("dest = \"%s\", src = \"%s\", n = 2  -->  ", s11, s12);
 	return_value = ft_strncat(s11, s12, 2);
-	printf("dest = \"%s\", src = \"%s\", returned value = %s\n", s11, s12, return_value == s11 ? "OK" : "KO");
+	printf("dest = \"%s\", returned value = %s\n", s11, return_value == s11 ? "OK" : "KO");
 }
