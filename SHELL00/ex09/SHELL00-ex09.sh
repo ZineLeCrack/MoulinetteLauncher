97086b11ce464708aae7db3711dcb79e9e93cae1
ZineLeCrack <rlebaill27@gmail.com>
2026-04-01 10:22:33 +0200

    SHELL00 ex09 repaired

--- SHELL00/ex09/SHELL00-ex09.sh
+++ SHELL00/ex09/SHELL00-ex09.sh
@@ -10,9 +10,9 @@ RESET="\033[0m"
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 src_dir="ex09"
 
-file -m "$src_dir/ft_magic" "$script_dir/test_file0" > "$script_dir/user_output"
-file -m "$src_dir/ft_magic" "$script_dir/test_file1" >> "$script_dir/user_output"
-file -m "$src_dir/ft_magic" "$script_dir/test_file2" >> "$script_dir/user_output"
+file -m "$src_dir/ft_magic" "$script_dir/test_file0" | awk '{print $2, $3}' > "$script_dir/user_output"
+file -m "$src_dir/ft_magic" "$script_dir/test_file1" | awk '{print $2, $3}' >> "$script_dir/user_output"
+file -m "$src_dir/ft_magic" "$script_dir/test_file2" | awk '{print $2, $3}' >> "$script_dir/user_output"
 
 diff -au --color=always "$script_dir/user_output" "$script_dir/expected_output"
 
