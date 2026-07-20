ce603cba9056a12573b82a86a985542c63a322a6
ZineLeCrack <rlebaill27@gmail.com>
2026-07-20 17:50:56 +0200

    fix ft_hexdump

--- C10/ex03/C10-ex03.sh
+++ C10/ex03/C10-ex03.sh
@@ -9,8 +9,8 @@ RESET="\033[0m"
 
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 
-executable="./ft_hexdump"
 src_dir="ex03"
+executable="$PWD/""$src_dir/""ft_hexdump"
 
 cd "$src_dir"
 make > "$script_dir/user_output"
@@ -24,83 +24,84 @@ if [[ $? -ne 0 ]]; then
 
 else
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" > "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	cd "$script_dir"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" > "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" "$script_dir/no_perm.txt" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" "no_perm.txt" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" < "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" < "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" -C "$script_dir/error.txt" "$script_dir/test.txt" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" -C "error.txt" "test.txt" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	chmod 000 "$script_dir/no_perm.txt"
+	chmod 000 "no_perm.txt"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" -C "$script_dir/test.txt" "$script_dir/no_perm.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" -C "test.txt" "no_perm.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" "$script_dir/no_perm.txt" "$script_dir/test.txt" "$script_dir/error.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" "no_perm.txt" "test.txt" "error.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" -C "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" -C "test.txt" "test.txt" "test.txt" "test.txt" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" "test.txt" "test.txt" "test.txt" "test.txt" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	echo -n "stdin  : " > "$script_dir/user_stdin_output"
-	echo -n "stderr : " > "$script_dir/user_stderr_output"
-	"$executable" -C "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" "$script_dir/test.txt" >> "$script_dir/user_stdin_output" 2>> "$script_dir/user_stderr_output"
-	cat "$script_dir/user_stdin_output" "$script_dir/user_stderr_output" >> "$script_dir/user_output"
-	echo "Exit status:" $? >> "$script_dir/user_output"
+	echo -n "stdin  : " > "user_stdin_output"
+	echo -n "stderr : " > "user_stderr_output"
+	"$executable" -C "test.txt" "test.txt" "test.txt" "test.txt" "test.txt" >> "user_stdin_output" 2>> "user_stderr_output"
+	cat "user_stdin_output" "user_stderr_output" >> "user_output"
+	echo "Exit status:" $? >> "user_output"
 
-	echo >> "$script_dir/user_output"
+	echo >> "user_output"
 
-	chmod 644 "$script_dir/no_perm.txt"
+	chmod 644 "no_perm.txt"
 
-	diff -au --color=always "$script_dir/user_output" "$script_dir/expected_output"
+	diff -au --color=always "user_output" "expected_output"
 
 	if [[ $? -ne 0 ]]; then
 
@@ -108,9 +109,9 @@ else
 		echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FAILURE <<<<<<<<<<<<<<<<<<<<<<<<<<<$RESET"
 		echo -e "${RED}Diff KO :(${RESET}"
 
-		valgrind "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "All heap blocks were freed -- no leaks are possible" && \
-		valgrind "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "ERROR SUMMARY: 0 errors from 0 contexts" && \
-		valgrind --track-fds=yes "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "FILE DESCRIPTORS: 3 open (3 std) at exit."
+		valgrind "$executable" -c 42 "test.txt" 2>&1 | grep -q "All heap blocks were freed -- no leaks are possible" && \
+		valgrind "$executable" -c 42 "test.txt" 2>&1 | grep -q "ERROR SUMMARY: 0 errors from 0 contexts" && \
+		valgrind --track-fds=yes "$executable" -c 42 "test.txt" 2>&1 | grep -q "FILE DESCRIPTORS: 3 open (3 std) at exit."
 
 		if [[ $? -ne 0 ]]; then
 
@@ -124,21 +125,21 @@ else
 
 	else
 
-		valgrind "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "All heap blocks were freed -- no leaks are possible" && \
-		valgrind "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "ERROR SUMMARY: 0 errors from 0 contexts" && \
-		valgrind --track-fds=yes "$executable" -c 42 "$script_dir/test.txt" 2>&1 | grep -q "FILE DESCRIPTORS: 3 open (3 std) at exit."
+		valgrind "$executable" -c 42 "test.txt" 2>&1 | grep -q "All heap blocks were freed -- no leaks are possible" && \
+		valgrind "$executable" -c 42 "test.txt" 2>&1 | grep -q "ERROR SUMMARY: 0 errors from 0 contexts" && \
+		valgrind --track-fds=yes "$executable" -c 42 "test.txt" 2>&1 | grep -q "FILE DESCRIPTORS: 3 open (3 std) at exit."
 
 		if [[ $? -ne 0 ]]; then
 
 			echo -e "$RED"
-			valgrind --track-fds=yes --trace-children=yes --leak-check=full --show-leak-kinds=all --track-origins=yes "$executable" -c 42 "$script_dir/test.txt"
+			valgrind --track-fds=yes --trace-children=yes --leak-check=full --show-leak-kinds=all --track-origins=yes "$executable" -c 42 "test.txt"
 			echo -e "$RESET"
 			echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FAILURE <<<<<<<<<<<<<<<<<<<<<<<<<<<$RESET"
 			echo -e "${RED}Memory check KO :(${RESET}"
 
 		else 
 
-			norminette -R CheckForbiddenSourceHeader | grep "Error" > "$script_dir/user_output"
+			norminette -R CheckForbiddenSourceHeader | grep "Error" > "user_output"
 
 			if [[ $? -eq 0 ]]; then
 
@@ -156,6 +157,7 @@ else
 
 	fi fi fi
 
+	rm -f "user_output" "user_stdin_output" "user_stderr_output"
+	cd "$OLDPWD"
 	make fclean
-	rm -f "$script_dir/user_output" "$script_dir/user_stdin_output" "$script_dir/user_stderr_output"
 fi
