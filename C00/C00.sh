d392f33606d928bfaa82df56cdffae961e1958e6
ZineLeCrack <rlebaill27@gmail.com>
2026-07-22 12:01:57 +0200

    C00: adding grade management and 'Nothing turn in' message no longer appear

--- C00/C00.sh
+++ C00/C00.sh
@@ -10,135 +10,189 @@ RESET="\033[0m"
 echo
 echo -e "$BLUE=========================     C00    ============================$RESET"
 
+final_grade=0
+end_grade=0
+
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 
 src_dir="ex00"
 
-echo
-
-echo -e "$MAGENTA==========================    ex00    ===========================$RESET"
-echo -e "$MAGENTA========================== ft_putchar ===========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA==========================    ex00    ===========================$RESET"
+	echo -e "$MAGENTA========================== ft_putchar ===========================$RESET"
+
 	bash "$script_dir/ex00/C00-ex00.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex01"
 
-echo
-
-echo -e "$MAGENTA=======================       ex01        =======================$RESET"
-echo -e "$MAGENTA======================= ft_print_alphabet =======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=======================       ex01        =======================$RESET"
+	echo -e "$MAGENTA======================= ft_print_alphabet =======================$RESET"
+
 	bash "$script_dir/ex01/C00-ex01.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex02"
 
-echo
-
-echo -e "$MAGENTA===================           ex02            ===================$RESET"
-echo -e "$MAGENTA=================== ft_print_reverse_alphabet ===================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA===================           ex02            ===================$RESET"
+	echo -e "$MAGENTA=================== ft_print_reverse_alphabet ===================$RESET"
+
 	bash "$script_dir/ex02/C00-ex02.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex03"
 
-echo
-
-echo -e "$MAGENTA=======================       ex03       ========================$RESET"
-echo -e "$MAGENTA======================= ft_print_numbers ========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=======================       ex03       ========================$RESET"
+	echo -e "$MAGENTA======================= ft_print_numbers ========================$RESET"
+
 	bash "$script_dir/ex03/C00-ex03.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex04"
 
-echo
-
-echo -e "$MAGENTA========================      ex04      =========================$RESET"
-echo -e "$MAGENTA======================== ft_is_negative =========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA========================      ex04      =========================$RESET"
+	echo -e "$MAGENTA======================== ft_is_negative =========================$RESET"
+
 	bash "$script_dir/ex04/C00-ex04.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex05"
 
-echo
-echo -e "$MAGENTA=========================     ex05      =========================$RESET"
-echo -e "$MAGENTA========================= ft_print_comb =========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=========================     ex05      =========================$RESET"
+	echo -e "$MAGENTA========================= ft_print_comb =========================$RESET"
+
 	bash "$script_dir/ex05/C00-ex05.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex06"
 
-echo
-echo -e "$MAGENTA=========================     ex06       ========================$RESET"
-echo -e "$MAGENTA========================= ft_print_comb2 ========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=========================     ex06       ========================$RESET"
+	echo -e "$MAGENTA========================= ft_print_comb2 ========================$RESET"
+
 	bash "$script_dir/ex06/C00-ex06.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex07"
 
-echo
-echo -e "$MAGENTA===========================   ex07    ===========================$RESET"
-echo -e "$MAGENTA=========================== ft_putnbr ===========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA===========================   ex07    ===========================$RESET"
+	echo -e "$MAGENTA=========================== ft_putnbr ===========================$RESET"
+
 	bash "$script_dir/ex07/C00-ex07.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex08"
 
-echo
-echo -e "$MAGENTA========================      ex08      =========================$RESET"
-echo -e "$MAGENTA======================== ft_print_combn =========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA========================      ex08      =========================$RESET"
+	echo -e "$MAGENTA======================== ft_print_combn =========================$RESET"
+
 	bash "$script_dir/ex08/C00-ex08.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
+
+echo -e $YELLOW"\n\nFinal grade: $final_grade"$RESET
