71cefee349b91e9d27eae030edcd24b175866da2
ZineLeCrack <rlebaill27@gmail.com>
2026-07-24 15:57:08 +0200

    C13: adding grade management and 'Nothing turn in' message no longer appear

--- C13/C13.sh
+++ C13/C13.sh
@@ -7,6 +7,9 @@ BLUE="\033[34;1m"
 MAGENTA="\033[35;1m"
 RESET="\033[0m"
 
+final_grade=0
+end_grade=0
+
 echo
 echo -e "$BLUE=========================     C13    ============================$RESET"
 
@@ -14,120 +17,162 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 
 src_dir="ex00"
 
-echo
-
-echo -e "$MAGENTA======================       ex00        ========================$RESET"
-echo -e "$MAGENTA====================== btree_create_node ========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA======================       ex00        ========================$RESET"
+	echo -e "$MAGENTA====================== btree_create_node ========================$RESET"
+
 	bash "$script_dir/ex00/C13-ex00.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex01"
 
-echo
-
-echo -e "$MAGENTA======================        ex01        =======================$RESET"
-echo -e "$MAGENTA====================== btree_apply_prefix =======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA======================        ex01        =======================$RESET"
+	echo -e "$MAGENTA====================== btree_apply_prefix =======================$RESET"
+
 	bash "$script_dir/ex01/C13-ex01.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex02"
 
-echo
-
-echo -e "$MAGENTA=======================       ex02        =======================$RESET"
-echo -e "$MAGENTA======================= btree_apply_infix =======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=======================       ex02        =======================$RESET"
+	echo -e "$MAGENTA======================= btree_apply_infix =======================$RESET"
+
 	bash "$script_dir/ex02/C13-ex02.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex03"
 
-echo
-
-echo -e "$MAGENTA=========================     ex03     ==========================$RESET"
-echo -e "$MAGENTA========================= ft_list_last ==========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=========================     ex03     ==========================$RESET"
+	echo -e "$MAGENTA========================= ft_list_last ==========================$RESET"
+
 	bash "$script_dir/ex03/C13-ex03.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex04"
 
-echo
-
-echo -e "$MAGENTA=======================       ex04        =======================$RESET"
-echo -e "$MAGENTA======================= btree_insert_data =======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=======================       ex04        =======================$RESET"
+	echo -e "$MAGENTA======================= btree_insert_data =======================$RESET"
+
 	bash "$script_dir/ex04/C13-ex04.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex05"
 
-echo
-
-echo -e "$MAGENTA=======================       ex05        =======================$RESET"
-echo -e "$MAGENTA======================= btree_search_item =======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=======================       ex05        =======================$RESET"
+	echo -e "$MAGENTA======================= btree_search_item =======================$RESET"
+
 	bash "$script_dir/ex05/C13-ex05.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex06"
 
-echo
-
-echo -e "$MAGENTA======================        ex06       ========================$RESET"
-echo -e "$MAGENTA====================== btree_level_count ========================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA======================        ex06       ========================$RESET"
+	echo -e "$MAGENTA====================== btree_level_count ========================$RESET"
+
 	bash "$script_dir/ex06/C13-ex06.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
 
 src_dir="ex07"
 
-echo
-
-echo -e "$MAGENTA=====================         ex07         ======================$RESET"
-echo -e "$MAGENTA===================== btree_apply_by_level ======================$RESET"
-
-if [[ ! -d "$src_dir" ]]; then
+if [[ -d "$src_dir" ]]; then
 	echo
-	echo -e "$RED>>>>>>>>>>>>>>>>>>>>>>>> Nothing turned in <<<<<<<<<<<<<<<<<<<<<<$RESET"
-	echo -e "${RED}KO :(${RESET}"
-else
+
+	echo -e "$MAGENTA=====================         ex07         ======================$RESET"
+	echo -e "$MAGENTA===================== btree_apply_by_level ======================$RESET"
+
 	bash "$script_dir/ex07/C13-ex07.sh"
+
+	grade=$?
+	echo -e $YELLOW"\nGrade = $grade"$RESET
+	if [[ $grade -eq 0 ]]; then
+		end_grade=1
+	fi
+	if [[ $end_grade -ne 1 ]]; then
+		final_grade=$((final_grade + grade))
+	fi
 fi
+
+echo -e $YELLOW"\n\nFinal grade: $final_grade"$RESET
